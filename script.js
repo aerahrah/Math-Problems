a206e3d784c38d51901f540e4a97ebf143fcf69f
aerahrah <61181505+aerahrah@users.noreply.github.com>
2022-02-11 14:44:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let thirdNum;
 let score = 0;
 let highscore = 0;
 let Tquestion = 0;
+let counter = 0;
 
 function question() {
   let RandomTwoPicker = [];
@@ -16,12 +17,14 @@ function question() {
     thirdNum = Math.trunc(Math.random() * 2);
     RandomTwoPicker[j] = thirdNum;
   }
+  //
+
   FirstNum = Math.trunc(
-    (Math.random() * 20 + 1) * (Math.round(Math.random()) ? 1 : -1)
+    (Math.random() * 17 + 8) * (Math.round(Math.random()) ? 1 : -1)
   );
 
   SecondNum = Math.trunc(
-    (Math.random() * 20 + 1) * (Math.round(Math.random()) ? 1 : -1)
+    (Math.random() * 23 + 1) * (Math.round(Math.random()) ? 1 : -1)
   );
 
   if (RandomTwoPicker[0] === 0) {
@@ -29,16 +32,21 @@ function question() {
     AnswerText = `${FirstNum} + ${SecondNum}`;
     //prompt(`${FirstNum} + ${SecondNum}`);
   } else {
-    CheckAnswer = FirstNum - SecondNum;
+    CheckAnswer = FirstNum + SecondNum;
     //prompt(`${FirstNum} - ${SecondNum}`);
-    AnswerText = `${FirstNum} - ${SecondNum}`;
+    AnswerText = `${FirstNum} + ${SecondNum}`;
   }
   document.querySelector('.number').textContent = AnswerText;
 }
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
+
 };
+
 document.querySelector('.next').addEventListener('click', function () {
+  counter += 1;
+  document.querySelector('.next').disabled = true;
+  document.querySelector('.title').textContent = `Question # ${counter}`;
   document.querySelector('.check').disabled = false;
   displayMessage('Start Answering...');
   document.querySelector('.guess').value = '';
@@ -52,25 +60,33 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('⛔️ No number!');
   } else if (document.querySelector('.number').textContent === '-') {
     displayMessage('Click start first');
-  } else if (guess === CheckAnswer) {
-    if (Tquestion < 25) {
+  } else if (guess == CheckAnswer) {
+    if (Tquestion < 10) {
       score++;
       Tquestion++;
+      document.querySelector('.title').textContent = `CORRECT ANSWER`;
       displayMessage('🎉 Correct Answer!');
       document.querySelector('.score').textContent = score;
       document.querySelector('.check').disabled = true;
+      document.querySelector('.next').disabled = false;
       document.querySelector('body').style.backgroundColor = '#1E6F5C';
     } else {
       displayMessage('🎉 Click Start to Start Again');
       document.querySelector('.score').textContent = score;
       document.querySelector('.highscore').textContent = score;
       document.querySelector('body').style.backgroundColor = '#1E6F5C';
-      if (score === 25) {
-        document.querySelector('.between').textContent = 'dagitamann';
+      if (score === 10) {
+        document.getElementById("lock").style.display = "block";
+        document.getElementById("unlock").style.display = "none";
+        document.getElementById("unlock1").style.display = "none";
+        document.getElementById("lock1").style.display = "none";
       }
     }
-  } else if (guess !== CheckAnswer) {
-    if (Tquestion < 25) {
+  } else if (guess != CheckAnswer) {
+    if (Tquestion < 10) {
+      document.querySelector('.check').disabled = true;
+      document.querySelector('.next').disabled = false;
+      document.querySelector('.title').textContent = `WRONG ANSWER`;
       document.querySelector('body').style.backgroundColor = '#7D0633';
       displayMessage(
         `❌ Wrong answer... Correct Asnwer for: ${AnswerText} is ${CheckAnswer}`
@@ -82,21 +98,24 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
       document.querySelector('.highscore').textContent = score;
       document.querySelector('body').style.backgroundColor = '#1E6F5C';
-      if (score === 25) {
-        document.querySelector('.between').textContent = 'dagitamann';
+      if (score == 10) {
+        document.getElementById("lock").style.display = "block";
+        document.getElementById("unlock").style.display = "none";
       }
     }
   }
 });
 
 document.querySelector('.start').addEventListener('click', function () {
+  counter = 1;
   score = 0;
   Tquestion = 0;
   question();
   displayMessage('Start Answering...');
+  document.querySelector('.next').disabled = true;
+  document.querySelector('.title').textContent = `Question # ${counter}`;
   document.querySelector('.between').textContent = 'Credits: Ivan';
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '45rem';
 });
